2020550e1d5e6a3ff7f834445f71b84bfe443fd1
Eugene Sazonov <saz.vit@gmail.com>
2022-09-01 00:49:58 +0200

    Initial commit

---
+++ src/test/java/org/zzy/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,11 @@
+package org.zzy.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
